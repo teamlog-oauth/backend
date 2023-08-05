5f746f7a200fd72a0ebc7aeac4c293f38e3cb8f6
PleBea <bini060315kim@gmail.com>
2023-08-05 23:07:36 +0900

    chore: Remove unused routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,42 +1,42 @@
 import {
   Controller,
-  Get,
-  Post,
   Body,
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AccessGuard } from 'src/auth/guards/access.guard';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Patch(':uuid')
+  @UseGuards(AccessGuard)
+  update(
+    @Param('uuid') uuid: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Req() req: Request,
+  ) {
+    if (uuid !== req.user.uuid)
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+
+    return this.userService.update(uuid, updateUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
+  @Delete(':uuid')
+  @UseGuards(AccessGuard)
+  remove(@Req() req: Request, @Param('uuid') uuid: string) {
+    if (uuid !== req.user.uuid)
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id);
+    return this.userService.remove(uuid);
   }
 }
